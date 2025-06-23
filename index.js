7eed9e34ccce6ddf99bc50fb9bafadf135564297
MOHAMMED HISHAM <mohammedhishamkn000@gmail.com>
2025-06-23 15:47:01 +0530

    main section complete

---
+++ index.js
@@ -0,0 +1,11 @@
+function show(){
+    
+    const show=document.getElementById("menuItems");
+    show.style.display="block";
+}
+function close(){
+    console.log("clicked");
+    const close=document.getElementsById("menuItems");
+    close.style.display="none";
+}
+
